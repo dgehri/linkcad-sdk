57a6a1832a761ee08185ca03f2f8374fca6c5f85
Daniel Gehriger <gehriger@linkcad.com>
2025-06-14 11:45:46 +0200

    Simplify plugins

--- examples/tlcin/TlcReaderPlugIn.cpp
+++ examples/tlcin/TlcReaderPlugIn.cpp
@@ -6,12 +6,39 @@
 // may not be redistributed, modified, copied in any way, by any means without
 // written permission of Gehriger Engineering.
 //------------------------------------------------------------------------------
-#include "TlcReaderPlugIn.h"
 #include "TlcReader.h"
 #include <lc/lic/License.h>
-
+#include <lc/plugin/IReaderPlugIn.h>
 
 namespace lc::format::tlcin {
+
+//------------------------------------------------------------------------------
+// IPlugIn implementation
+//------------------------------------------------------------------------------
+class TlcReaderPlugIn : public lc::plugin::IPlugIn
+{
+public:
+    // Load plug-in
+    virtual bool load(const plugin::IPlugInContext* context, void* /* module */);
+
+    // Unload plug-in
+    virtual bool unload();
+
+private:
+    struct Reader : lc::plugin::IReaderPlugIn
+    {
+        // Describe the file format
+        virtual void describeFormat(plugin::IFormat* format) const;
+
+        // Configure file format
+        virtual void configureFormat() const;
+
+        // Create reader instance
+        virtual lc::plugin::IReader* createInstance() const;
+    };
+
+    Reader reader_;
+};
 
 //------------------------------------------------------------------------------
 bool TlcReaderPlugIn::load(const plugin::IPlugInContext* context, void* /*module*/)
@@ -49,5 +76,10 @@ plugin::IReader* TlcReaderPlugIn::Reader::createInstance() const
 {
     return new TlcReader;
 }
+
+//------------------------------------------------------------------------------
+// Declare plug-in
+//------------------------------------------------------------------------------
+DECLARE_PLUGIN(TlcReaderPlugIn);
 
 }  // namespace lc::format::tlcin
--- examples/tlcout/TlcWriterPlugIn.cpp
+++ examples/tlcout/TlcWriterPlugIn.cpp
@@ -5,11 +5,39 @@
 // may not be redistributed, modified, copied in any way, by any means without
 // written permission of Gehriger Engineering.
 //------------------------------------------------------------------------------
-#include "TlcWriterPlugIn.h"
 #include "TlcWriter.h"
 #include <lc/lic/License.h>
+#include <lc/plugin/IWriterPlugIn.h>
 
 namespace lc::format::tlcout {
+
+//------------------------------------------------------------------------------
+// IPlugIn implementation
+//------------------------------------------------------------------------------
+class TlcWriterPlugIn : public lc::plugin::IPlugIn
+{
+public:
+    // Load plug-in
+    virtual bool load(const plugin::IPlugInContext* context, void* /* module */);
+
+    // Unload plug-in
+    virtual bool unload();
+
+private:
+    struct Writer : lc::plugin::IWriterPlugIn
+    {
+        // Describe the file format
+        virtual void describeFormat(plugin::IFormat* format) const;
+
+        // Configure file format
+        virtual void configureFormat() const;
+
+        // Create writer instance
+        virtual lc::plugin::IWriter* createInstance() const;
+    };
+
+    Writer writer_;
+};
 
 //------------------------------------------------------------------------------
 bool TlcWriterPlugIn::load(const plugin::IPlugInContext* context, void* /*module*/)
@@ -50,5 +78,10 @@ plugin::IWriter* TlcWriterPlugIn::Writer::createInstance() const
 {
     return new TlcWriter;
 }
+
+//------------------------------------------------------------------------------
+// Declare plug-in
+//------------------------------------------------------------------------------
+DECLARE_PLUGIN(TlcWriterPlugIn);
 
 }  // namespace lc::format::tlcout
